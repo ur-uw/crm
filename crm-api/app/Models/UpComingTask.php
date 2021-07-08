0f05b6351264598920b597daaed068dd8b8a3df7
ur-uw <mfanha8030@gmail.com>
2021-07-08 11:16:56 +0300

    Add fillable properties to models

--- crm-api/app/Models/UpComingTask.php
+++ crm-api/app/Models/UpComingTask.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class UpComingTask extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'completed',
+        'approved',
+        'taskId',
+        'waiting',
+    ];
 }

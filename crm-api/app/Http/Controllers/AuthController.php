d43cf7d83bd7e207aa80e403d30e4b40e9744fa0
ur-uw <mfanha8030@gmail.com>
2021-07-19 12:20:40 +0300

    Make basic auth scaffold

---
+++ crm-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+    }
+    public function register(RegisterRequest $request)
+    {
+    }
+}

34baaa0deba3308e4da5ccbc7489c228ae8c6745
ur-uw <mfanha8030@gmail.com>
2021-07-19 20:51:55 +0300

    Add laravel/sanctum

--- crm-api/app/Http/Controllers/AuthController.php
+++ crm-api/app/Http/Controllers/AuthController.php
@@ -4,14 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        return response()->json([
+            'message' => 'Login Success',
+            'user' => $user,
+            'token' => $token,
+        ]);
     }
     public function register(RegisterRequest $request)
     {
+        try {
+            $user = User::create($request->validated());
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'message' => 'User created',
+                'user' => $user,
+                'token' => $token,
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => $exception->getMessage(),
+            ]);
+        }
     }
 }

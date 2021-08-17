2c2c4fd41378488597d71067ce9d7120e67be394
ur-uw <16156658+ur-uw@users.noreply.github.com>
2021-08-17 19:59:25 +0300

    feat: add logged in user info to settings.account page form

--- crm-vue/src/views/settings/AccountSettings.vue
+++ crm-vue/src/views/settings/AccountSettings.vue
@@ -3,7 +3,7 @@
     <h3 class="fw-bold mb-5">Account Settings</h3>
     <n-divider />
     <h5 class="fw-bold mt-5">Personal Information</h5>
-    <n-form class="mt-5">
+    <n-form ref="formRef" :model="model" :rules="rules" class="mt-5">
       <div class="avatar-wrapper mb-3 px-3 d-flex align-items-center justify-content-start">
         <n-form-item>
           <n-avatar
@@ -30,25 +30,42 @@
 
       <n-grid class="px-3" :x-gap="15" :y-gap="15" cols="2 xs:1 s:1">
         <n-grid-item>
-          <n-form-item label="First Name">
-            <n-input class="input-field" placeholder="" size="large"></n-input>
+          <n-form-item path="firstName" label="First Name">
+            <n-input
+              v-model:value="model.firstName"
+              class="input-field"
+              placeholder=""
+              size="large"
+            ></n-input>
           </n-form-item>
         </n-grid-item>
         <n-grid-item>
-          <n-form-item label="Last Name">
-            <n-input class="input-field" placeholder="" size="large"></n-input>
+          <n-form-item path="lastName" label="Last Name">
+            <n-input
+              v-model:value="model.lastName"
+              class="input-field"
+              placeholder=""
+              size="large"
+            ></n-input>
           </n-form-item>
         </n-grid-item>
       </n-grid>
-      <n-form-item class="px-3" label="Email Address">
-        <n-input disabled class="input-field" placeholder="johndoe@email.com" size="large">
+      <n-form-item path="email" class="px-3" label="Email Address">
+        <n-input
+          v-model:value="model.email"
+          disabled
+          class="input-field"
+          placeholder="johndoe@email.com"
+          size="large"
+        >
           <template #suffix>
             <n-icon><MailIcon /></n-icon>
           </template>
         </n-input>
       </n-form-item>
-      <n-form-item class="px-3" label="Password">
+      <n-form-item path="password" class="px-3" label="Password">
         <n-input
+          v-model:value="model.password"
           class="input-field"
           show-password-toggle
           type="password"
@@ -108,9 +125,11 @@
 
 <script lang="ts">
   import { Mail28Regular as MailIcon } from '@vicons/fluent'
-  import { computed, defineComponent } from 'vue'
+  import { computed, defineComponent, ref, watch } from 'vue'
   import { useStore } from '@/use/useStore'
   import { User } from '@/interfaces/User'
+  import { FormItemRule } from 'naive-ui'
+  import { validEmail } from '@/utils/helpers'
 
   export default defineComponent({
     name: 'AccountSettings',
@@ -120,9 +139,64 @@
     setup() {
       // INITIALIZE STORE
       const store = useStore()
+      // VARIABLES
       const currentUser = computed<User | null>(() => store.getters.getCurrentUser)
+
+      let modelValue = ref({
+        firstName: currentUser?.value?.name,
+        lastName: currentUser?.value?.name,
+        email: currentUser?.value?.email,
+        password: ''
+      })
+      const formRef = ref(null)
+      const formModel = ref(modelValue.value)
+
+      // Form Rules
+      const formRules = {
+        email: {
+          required: true,
+          validator: (rule: FormItemRule, value: string) => {
+            if (!value) {
+              return new Error('Pleas enter your email')
+            } else if (!validEmail(value)) {
+              return Error('Please enter valid email address')
+            }
+            return true
+          },
+          trigger: ['input', 'blur']
+        },
+        password: {
+          required: true,
+          validator: (rule: FormItemRule, val: string) => {
+            if (val.length > 0 && val.length < 8) {
+              return Error('Password must be at least 8 chars')
+            } else if (val.length === 0 || val === null) {
+              return Error('Please enter your password')
+            }
+          }
+        },
+        firstName: {
+          required: true,
+          message: 'Please enter your first name'
+        },
+        lastName: {
+          required: true,
+          message: 'Please enter your last name'
+        }
+      }
+
+      watch(currentUser, (newVal: User | null) => {
+        if (newVal !== null) {
+          modelValue.value.email = newVal.email
+          modelValue.value.firstName = newVal.name
+          modelValue.value.lastName = newVal.name
+        }
+      })
       return {
-        user: currentUser
+        user: currentUser,
+        formRef,
+        model: formModel,
+        rules: formRules
       }
     }
   })
---
+++ crm-vue/src/views/settings/SettingsNotifications.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>Notifications</h3>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'SettingsNotifications'
+  })
+</script>
+<style lang="scss" scoped></style>
